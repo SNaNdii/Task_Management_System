3f8726559d65771d1f4475ab8f53a53ce0e620e0
SNaNdii <singhnandita73@gmail.com>
2024-03-11 22:34:12 +0530

    Continue Home page

--- task-management-system/src/components/pages/home/AddTaskDemo.jsx
+++ task-management-system/src/components/pages/home/AddTaskDemo.jsx
@@ -29,13 +29,12 @@ export const AddTaskDemo = () => {
                         <Spacer/>
                         <TextBox>
                                 <MidBox>
-                                        <Text fontSize='xl' as='b' color='#f14b39' >Clear your mind</Text>
-                                        <br/>
-                                        <Text fontSize='5xl' as='b'>The fastest way to get tasks out of your head.</Text>
-                                        <GreyText>Type just about anything into the task field and Todoist’s one-of-its-kind natural language recognition will instantly fill your to-do list.</GreyText>
+                                        
                                 </MidBox>
                         </TextBox>
                 </Wrap>
+                <Text>“Todoist makes it easy to go as simple or as complex as you want”</Text>
+                <Text>– The Verge</Text>
                 {/* </NavBox> */}
         </>
 };
---
+++ task-management-system/src/components/pages/home/Explore.jsx
@@ -0,0 +1,7 @@
+import { Text } from "@chakra-ui/react"
+
+export const Explore = () => {
+    return <>
+        <Text>Explore all Todoist has to offer</Text>
+    </>
+}
