1204cf2677ece5c0f4c1210970b8ae9711bb85e7
SNaNdii <singhnandita73@gmail.com>
2024-03-14 15:29:49 +0530

    Footer Start

--- task-management-system/src/components/Footer.jsx
+++ task-management-system/src/components/Footer.jsx
@@ -1,3 +1,9 @@
+import { Flex } from "@chakra-ui/react"
+import "./css/footer.css"
 export const Footer = () => {
-    
+    return <>
+        <Flex className="footer">
+            njdnj
+        </Flex>
+    </>
 }
