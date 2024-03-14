24becfb943fe1c3f7eea66636d8c985bc4a2c9a4
SNaNdii <singhnandita73@gmail.com>
2024-03-14 13:44:29 +0530

    Footer Start

--- task-management-system/src/components/pages/home/TaskTemplete.jsx
+++ task-management-system/src/components/pages/home/TaskTemplete.jsx
@@ -1,10 +1,11 @@
-import {ImgBg} from '../../styledComponent/HomeComp'
 import '../../css/home.css'
+import { CommonButton } from '../../styledComponent/CommonButton'
 
 export const TaskTemplate = () => {
     return <>
         <div className='bgImage'>
-            
+            <></>
+            <CommonButton>Start for free</CommonButton>
         </div>
 
         
