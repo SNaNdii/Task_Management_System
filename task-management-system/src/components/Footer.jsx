c430bb46adc6e35d72dfb86f6793788ba59d0773
SNaNdii <singhnandita73@gmail.com>
2024-03-15 20:59:00 +0530

    Footer Complete

--- task-management-system/src/components/Footer.jsx
+++ task-management-system/src/components/Footer.jsx
@@ -1,9 +1,9 @@
-import { Center, Divider, Flex, Grid, GridItem, Icon, List, ListItem, Text } from "@chakra-ui/react"
+import { Box, Center, Heading, Divider, Flex, Grid, GridItem, Icon, Image, List, ListItem, Spacer, Text } from "@chakra-ui/react"
 import "./css/footer.css"
 import { GreyText, TextBox } from "./styledComponent/HomeComp"
 import { CommonButton } from "./styledComponent/CommonButton"
-import { Logo } from "./styledComponent/NavBox"
 import {FaFacebook , FaInstagram, FaTwitter, FaYoutube} from 'react-icons/fa'
+import { ListIt , ListMain } from "./styledComponent/FooterComp"
 
 export const Footer = () => {
     return <div className="footer">
@@ -14,52 +14,52 @@ export const Footer = () => {
                 <CommonButton>Start for free</CommonButton>
             </TextBox>
         </Center>
-        <Divider my={4} borderColor="gray.600" />
+        <Divider my={10} borderColor="gray.500" />
 
-        <Flex>
+        <Flex className="footer_box" >
 
-            <Grid templateColumns='repeat(5, 1fr)'>
-                <GridItem>
-                    <Logo src="https://ucarecdn.com/d891cfcc-c46d-437f-b176-c5325e93ca55/-/format/auto/-/progressive/yes/-/preview/480x480/"/>
-                </GridItem>
+            <Box className="footer_img">
+                <img  src="https://ucarecdn.com/d891cfcc-c46d-437f-b176-c5325e93ca55/-/format/auto/-/progressive/yes/-/preview/480x480/"/>
+                <Text fontSize='xl'>Join millions of people who organize work and life with Todoist.</Text>
+            </Box>
 
-                <GridItem>
-                    <List>
-                        <ListItem> <Text as='b'>Features</Text> </ListItem>
-                        <ListItem>How it works</ListItem>
-                        <ListItem>For Team</ListItem>
-                        <ListItem>Pricing</ListItem>
-                    </List>
-                </GridItem>
+            <Spacer/>
 
-                <GridItem>
-                    <List>
-                        <ListItem> <Text as='b'>Resources</Text> </ListItem>
-                        <ListItem>How it works</ListItem>
-                        <ListItem>For Team</ListItem>
-                        <ListItem>Pricing</ListItem>
-                    </List>
-                </GridItem>
-
-                <GridItem>
-                    <List>
-                        <ListItem> <Text as='b'>Company</Text> </ListItem>
-                        <ListItem>How it works</ListItem>
-                        <ListItem>For Team</ListItem>
-                        <ListItem>Pricing</ListItem>
-                    </List>
-                </GridItem>
-
-                <GridItem>
-                    <List>
-                        <ListItem><Icon as={FaFacebook} boxSize={6} color="black" /></ListItem>
-                        <ListItem><Icon as={FaInstagram} boxSize={6} color="black" /></ListItem>
-                        <ListItem><Icon as={FaTwitter} boxSize={6} color="black" /></ListItem>
-                        <ListItem><Icon as={FaYoutube} boxSize={6} color="black" /></ListItem>
-                    </List>
-                </GridItem>
-
-            </Grid>
+            <ListMain>
+                <ListIt> <Text as='b'>Features</Text> </ListIt>
+                <ListIt>How it works</ListIt>
+                <ListIt>For Team</ListIt>
+                <ListIt>Pricing</ListIt>
+                <ListIt>Templates</ListIt>
+            </ListMain>
+            <Spacer/>
+            <ListMain>
+                <ListIt> <Text as='b'>Resources</Text> </ListIt>
+                <ListIt>Download Apps</ListIt>
+                <ListIt>Help Center</ListIt>
+                <ListIt>Productivity Methods</ListIt>
+                <ListIt>Integrations</ListIt>
+                <ListIt>Channel Partners</ListIt>
+                <ListIt>Developer API</ListIt>
+                <ListIt>Status</ListIt>
+            </ListMain>
+            <Spacer/>
+            <ListMain>
+                <ListIt> <Text as='b'>Company</Text> </ListIt>
+                <ListIt>About Us</ListIt>
+                <ListIt>Careers</ListIt>
+                <ListIt>Inspiration Hub</ListIt>
+                <ListIt>Press</ListIt>
+                <ListIt>Twist</ListIt>
+            </ListMain>
+            <Spacer/>
+            <ListMain>
+                <ListIt><Icon as={FaFacebook} boxSize={6} color="black" /></ListIt>
+                <ListIt><Icon as={FaInstagram} boxSize={6} color="black" /></ListIt>
+                <ListIt><Icon as={FaTwitter} boxSize={6} color="black" /></ListIt>
+                <ListIt><Icon as={FaYoutube} boxSize={6} color="black" /></ListIt>
+            </ListMain>
         </Flex>
+        <Box className="last"></Box>
     </div>
 }
---
+++ task-management-system/src/components/styledComponent/FooterComp.jsx
@@ -0,0 +1,10 @@
+import { List, ListItem } from "@chakra-ui/react";
+import styled from "@emotion/styled";
+
+export const ListIt = styled(ListItem)`
+    margin-bottom: 10px;
+`
+
+export const ListMain = styled(List)`
+    text-align: start;
+`
