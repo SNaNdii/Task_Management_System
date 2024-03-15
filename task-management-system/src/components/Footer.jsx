43d478db6c6b1ded630a7613a188aa840b28869d
SNaNdii <singhnandita73@gmail.com>
2024-03-15 14:33:06 +0530

    Footer Complete

--- task-management-system/src/components/Footer.jsx
+++ task-management-system/src/components/Footer.jsx
@@ -1,8 +1,10 @@
-import { Center, Divider, Flex, Grid, GridItem, List, ListItem, Text } from "@chakra-ui/react"
+import { Center, Divider, Flex, Grid, GridItem, Icon, List, ListItem, Text } from "@chakra-ui/react"
 import "./css/footer.css"
 import { GreyText, TextBox } from "./styledComponent/HomeComp"
 import { CommonButton } from "./styledComponent/CommonButton"
-import { Logo, NavButton } from "./styledComponent/NavBox"
+import { Logo } from "./styledComponent/NavBox"
+import {FaFacebook , FaInstagram, FaTwitter, FaYoutube} from 'react-icons/fa'
+
 export const Footer = () => {
     return <div className="footer">
         <Center>
@@ -15,15 +17,15 @@ export const Footer = () => {
         <Divider my={4} borderColor="gray.600" />
 
         <Flex>
-            
-            <Grid templateColumns='repeat(4, 1fr)'>
+
+            <Grid templateColumns='repeat(5, 1fr)'>
                 <GridItem>
                     <Logo src="https://ucarecdn.com/d891cfcc-c46d-437f-b176-c5325e93ca55/-/format/auto/-/progressive/yes/-/preview/480x480/"/>
                 </GridItem>
 
                 <GridItem>
                     <List>
-                        <ListItem>Features</ListItem>
+                        <ListItem> <Text as='b'>Features</Text> </ListItem>
                         <ListItem>How it works</ListItem>
                         <ListItem>For Team</ListItem>
                         <ListItem>Pricing</ListItem>
@@ -32,7 +34,7 @@ export const Footer = () => {
 
                 <GridItem>
                     <List>
-                        <ListItem>Features</ListItem>
+                        <ListItem> <Text as='b'>Resources</Text> </ListItem>
                         <ListItem>How it works</ListItem>
                         <ListItem>For Team</ListItem>
                         <ListItem>Pricing</ListItem>
@@ -41,10 +43,19 @@ export const Footer = () => {
 
                 <GridItem>
                     <List>
-                        <ListItem>Features</ListItem>
+                        <ListItem> <Text as='b'>Company</Text> </ListItem>
                         <ListItem>How it works</ListItem>
                         <ListItem>For Team</ListItem>
                         <ListItem>Pricing</ListItem>
+                    </List>
+                </GridItem>
+
+                <GridItem>
+                    <List>
+                        <ListItem><Icon as={FaFacebook} boxSize={6} color="black" /></ListItem>
+                        <ListItem><Icon as={FaInstagram} boxSize={6} color="black" /></ListItem>
+                        <ListItem><Icon as={FaTwitter} boxSize={6} color="black" /></ListItem>
+                        <ListItem><Icon as={FaYoutube} boxSize={6} color="black" /></ListItem>
                     </List>
                 </GridItem>
 
