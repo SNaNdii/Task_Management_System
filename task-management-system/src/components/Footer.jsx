3140ada6e9d12adade807b48ea74566202e2216d
SNaNdii <singhnandita73@gmail.com>
2024-03-15 14:12:33 +0530

    Footer Complete

--- task-management-system/src/components/Footer.jsx
+++ task-management-system/src/components/Footer.jsx
@@ -1,9 +1,54 @@
-import { Flex } from "@chakra-ui/react"
+import { Center, Divider, Flex, Grid, GridItem, List, ListItem, Text } from "@chakra-ui/react"
 import "./css/footer.css"
+import { GreyText, TextBox } from "./styledComponent/HomeComp"
+import { CommonButton } from "./styledComponent/CommonButton"
+import { Logo, NavButton } from "./styledComponent/NavBox"
 export const Footer = () => {
-    return <>
-        <Flex className="footer">
-            njdnj
+    return <div className="footer">
+        <Center>
+            <TextBox>
+                <Text fontSize='5xl' as='b'>Gain calmness and clarity with the world’s most beloved productivity app</Text>
+                <GreyText>337,000+ ★★★★★ reviews on Google Play and App Store</GreyText>
+                <CommonButton>Start for free</CommonButton>
+            </TextBox>
+        </Center>
+        <Divider my={4} borderColor="gray.600" />
+
+        <Flex>
+            
+            <Grid templateColumns='repeat(4, 1fr)'>
+                <GridItem>
+                    <Logo src="https://ucarecdn.com/d891cfcc-c46d-437f-b176-c5325e93ca55/-/format/auto/-/progressive/yes/-/preview/480x480/"/>
+                </GridItem>
+
+                <GridItem>
+                    <List>
+                        <ListItem>Features</ListItem>
+                        <ListItem>How it works</ListItem>
+                        <ListItem>For Team</ListItem>
+                        <ListItem>Pricing</ListItem>
+                    </List>
+                </GridItem>
+
+                <GridItem>
+                    <List>
+                        <ListItem>Features</ListItem>
+                        <ListItem>How it works</ListItem>
+                        <ListItem>For Team</ListItem>
+                        <ListItem>Pricing</ListItem>
+                    </List>
+                </GridItem>
+
+                <GridItem>
+                    <List>
+                        <ListItem>Features</ListItem>
+                        <ListItem>How it works</ListItem>
+                        <ListItem>For Team</ListItem>
+                        <ListItem>Pricing</ListItem>
+                    </List>
+                </GridItem>
+
+            </Grid>
         </Flex>
-    </>
+    </div>
 }
