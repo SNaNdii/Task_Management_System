b58a39e7bb03eba6f0e190481c2bc7868aa9e1e1
SNaNdii <singhnandita73@gmail.com>
2024-03-19 22:54:29 +0530

    Added Db json file

--- task-management-system/src/routes/TodoApp.jsx
+++ task-management-system/src/routes/TodoApp.jsx
@@ -2,6 +2,7 @@ import {Routes , Route} from 'react-router-dom'
 import { Login } from '../components/pages/login/Login'
 import {Home} from '../components/pages/home/Home'
 import { NavBar } from '../components/NavBar'
+import { Signup } from '../components/pages/signup/Signup'
 
 export const TodoApp = () => {
 
@@ -10,6 +11,7 @@ export const TodoApp = () => {
         <Routes>
             <Route path='/' element={<Home/>}></Route>
             <Route path='/login' element={<Login/>}></Route>
+            <Route path='/signup' element={<Signup/>}></Route>
         </Routes>
     </>
 }
